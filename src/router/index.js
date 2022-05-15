edac9ac5ddbe5a47fe45d8698b33a68071752bca
kamil <kamiln022@gmail.com>
2022-05-15 20:52:39 +0200

    Corrected import path in router

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import { useCookies } from 'vue3-cookies';
 
-import Login from './../pages/Login';
-import Weather from './../pages/Weather';
+import Login from '@/pages/Login';
+import Weather from '@/pages/Weather';
 
 const { cookies } = useCookies();
 
