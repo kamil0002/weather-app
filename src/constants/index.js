87e0c47b3468444f2e7f24827168b4ca88a528a4
kamil0002 <70920250+kamil0002@users.noreply.github.com>
2022-05-17 13:56:16 +0200

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,4 +2,4 @@ export const LOGIN = 'admin';
 export const PASSWORD = 'admin';
 
 export const API_KEY = 'dd50186329e9cab2c9efc43429f5cdd5';
-export const API_REFRESH_RATE = 20;
+export const API_REFRESH_RATE = 60;
